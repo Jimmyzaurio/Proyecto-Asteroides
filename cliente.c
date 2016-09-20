a03e6f167e1536ceddaab1644f1e5993c70d3408
Jimmyzaurio <jsp.saucedo@gmail.com>
2016-09-20 17:40:52 -0500

    Add files via upload

--- cliente.c
+++ cliente.c
@@ -1,16 +1,17 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <math.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
-#define ANGULO (rand() % 22) + 30
-#define RADIO ((rand()) % 30) + 28
+#define ANGULO (rand() % 17) + 30
+#define RADIO ((rand()) % 17) + 28
 #define THETA_RADIANES (((double)theta)/180.0)
 
 int puerto = 7200;
@@ -34,25 +35,27 @@ int main() {
   bind(s, (struct sockaddr *)&client_addr, sizeof(client_addr));
 
   while (1) {
-    int off_x = rand()%800;
-    int off_y = rand()%600;
+    // Se genera un offset para que cada asteroide tenga una ubicación diferente
+    int off_x = rand() % 800;
+    int off_y = rand() % 600;
     int coord[2];
     
     coord[0] = -1;
+    // Enviamos un valor bandera, indicando que se mandarán las coordenadas del asteroide
     sendto(s, (char *)coord, 2 * sizeof(int), 0, (struct sockaddr *) &msg_to_server_addr, sizeof(msg_to_server_addr));
 
     int inix, iniy, ultx, ulty, theta = 0;
+    // Utilizamos un random para determinar el incremento del ángulo que se hará
+    // dicho incremento estará entre 30 y 46 grados. 
     for (; theta <= 360; theta += ANGULO) {
+      // Las funciones sin y cos reciben como parámetros los radianes a calcular
       coord[0] = RADIO * cos(M_PI*THETA_RADIANES) + off_x;
       coord[1] = RADIO * sin(M_PI*THETA_RADIANES) + off_y;
       
-      if (theta == 0)
-        inix = coord[0], iniy = coord[1];
-      else if (theta == 359)
-        ultx = coord[0], ulty = coord[1];
-
+      // Enviamos las coordenadas
       sendto(s, (char *)coord, 2 * sizeof(int), 0, (struct sockaddr *) &msg_to_server_addr, sizeof(msg_to_server_addr));
     }
+    // Esperamos un segundo para enviar el siguiente asteroide
     sleep(1);
   }
   return 0;
--- servidor.c
+++ servidor.c
@@ -1,68 +1,70 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <string.h>
-#include <X11/Xlib.h>
-#include <unistd.h>
+#include <arpa/inet.h>
 #include <math.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <X11/Xlib.h>
 
-unsigned long ObtieneColor(Display* dis, char* color_name){
-   Colormap cmap;
-   XColor color_cercano, color_verdadero;
+// Función que nos regresa el valor para asignar color a nuestra línea
+int setColor(Display* dis, char* color_name){
+  Colormap cmap = DefaultColormap(dis, 0);
 
-   cmap = DefaultColormap(dis, 0);
-   XAllocNamedColor(dis, cmap, color_name, &color_cercano, &color_verdadero);
+  XColor color_cercano, color_verdadero;
+  XAllocNamedColor(dis, cmap, color_name, &color_cercano, &color_verdadero);
 
-	return(color_cercano.pixel);
+  return(color_cercano.pixel);
 }
 
 int puerto = 7200;
 
-#define NUEVO_ASTEROIDE num[0] == -1
+#define NUEVO_ASTEROIDE coord[0] == -1
 
 int main() {
-	int ax = 0, ay = 0, num[2];
-	struct sockaddr_in server_addr, msg_to_client_addr;
-	Display *disp = NULL;
-    Window ventana;
-    XColor color;
+  struct sockaddr_in server_addr, msg_to_client_addr;
+  Display *disp = NULL;
+  Window ventana;
+  XColor color;
 
-	int s = socket(AF_INET, SOCK_DGRAM, 0);
+  int s = socket(AF_INET, SOCK_DGRAM, 0);
 
-	bzero((char*)&server_addr, sizeof(server_addr));
-	server_addr.sin_family      = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port        = htons(puerto);
+  bzero((char*)&server_addr, sizeof(server_addr));
+  server_addr.sin_family      = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port        = htons(puerto);
 
-	bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-	int clilen = sizeof(msg_to_client_addr);
+  int clilen = sizeof(msg_to_client_addr);
 
-	disp = XOpenDisplay(NULL);
-	ventana = XCreateSimpleWindow (disp, XDefaultRootWindow (disp), 100, 100, 800, 600, 1, 1,BlackPixel (disp, DefaultScreen(disp)));
-	XMapWindow (disp, ventana);
+  disp    = XOpenDisplay(NULL);
+  ventana = XCreateSimpleWindow (disp, XDefaultRootWindow (disp), 100, 100, 800, 600, 1, 1,BlackPixel (disp, DefaultScreen(disp)));
+  XMapWindow (disp, ventana);
 
-	int x = 0, y = 0;
-	int prevx = 0, prevy = 0;
-	while (1) {
-		int num[2];
-		recvfrom(s, (char*)num, sizeof(int) * 2, 0, (struct sockaddr*)&msg_to_client_addr, &clilen);
+  // Es necesario guardar el inicio del polígono para poder colocar la última línea del polígono
+  int inix = 0, iniy = 0, prevx = 0, prevy = 0;
+  while (1) {
+    int coord[2];
+    recvfrom(s, (char*)coord, sizeof(int) * 2, 0, (struct sockaddr*)&msg_to_client_addr, &clilen);
 
-		if (NUEVO_ASTEROIDE) {
-			XSetForeground (disp, XDefaultGC (disp, DefaultScreen(disp)), BlackPixel(disp, 0)^ObtieneColor(disp, "white"));
-			XDrawLine (disp,ventana,XDefaultGC (disp, DefaultScreen(disp)), x, y, ax, ay);
-			recvfrom(s, (char*)num, sizeof(int) * 2, 0, (struct sockaddr*)&msg_to_client_addr, &clilen);
-			x = ax = num[0];
-			y = ay = num[1];
-			//sleep(1);
-		}else{
-			XSetForeground (disp, XDefaultGC (disp, DefaultScreen(disp)), BlackPixel(disp, 0)^ObtieneColor(disp, "white"));
-			XDrawLine (disp,ventana,XDefaultGC (disp, DefaultScreen(disp)), ax, ay, num[0], num[1]);
+    // En caso de que sea el inicio de un nuevo asteroide, leemos la coordenada inicial
+    // Es necesario mencionar que la última línea del último polígono se pondrá en éste momento
+    if (NUEVO_ASTEROIDE) {
+      XSetForeground (disp, XDefaultGC (disp, DefaultScreen(disp)), setColor(disp, "green"));
+      XDrawLine (disp,ventana,XDefaultGC (disp, DefaultScreen(disp)), inix, iniy, prevx, prevy);
+      recvfrom(s, (char*)coord, sizeof(int) * 2, 0, (struct sockaddr*)&msg_to_client_addr, &clilen);
 
-			ax = num[0];
-			ay = num[1];
-		}
-	}
+      inix = prevx = coord[0];
+      iniy = prevy = coord[1];
+    } else {
+      XSetForeground (disp, XDefaultGC (disp, DefaultScreen(disp)), setColor(disp, "green"));
+      XDrawLine (disp,ventana,XDefaultGC (disp, DefaultScreen(disp)), prevx, prevy, coord[0], coord[1]);
+
+      prevx = coord[0];
+      prevy = coord[1];
+    }
+  }
 }
